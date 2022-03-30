eb11095ed8f3c8bf4eb483a46198c0733b764c0e
Andres <agdevices@gmail.com>
2022-03-29 21:50:03 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,544 @@
+const contractor = [
+    {
+     "_id": "6220d7cac39ee6cb02fbec2d",
+     "companyName": "FERRO CONSTRUCTION INC",
+     "qualifier": "FERRO FERNANDO",
+     "companyMail": "ferro_fernando@yahoo.com",
+     "companyPhone": "954-274-8915",
+     "companyAddress": "2542 MONROE ST",
+     "companyCounty": "HOLLYWOOD",
+     "companyState": "FLORIDA",
+     "companyZip": 33020,
+     "licenseType": "CGC",
+     "licenseNumber": "1510147",
+     "licenseIssued": null,
+     "licenseExpDate": null
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec2e",
+     "companyName": "MASTER BATH OF FLORIDA LLC",
+     "qualifier": "AREVALO ROLANDO",
+     "companyMail": "masterbathsfl@gmail.com",
+     "companyPhone": "305-804-8388",
+     "companyAddress": "8574 NW 54 ST",
+     "companyCounty": "DORAL",
+     "companyState": "FLORIDA",
+     "companyZip": 33166,
+     "licenseType": "CGC",
+     "licenseNumber": "1528673",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec2f",
+     "companyName": "CORAL GABLES FLOORS AND PAINTING INC",
+     "qualifier": "CARLOS LOPEZ",
+     "companyMail": "ariel-usa@hotmail.com",
+     "companyPhone": "305-431-4364",
+     "companyAddress": "6800 SW 40 ST #682",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33155,
+     "licenseType": "LICENSE",
+     "licenseNumber": "10BS00411",
+     "licenseIssued": null,
+     "licenseExpDate": null
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec30",
+     "companyName": "MR HOUSE FLOORING SERVICES INC",
+     "qualifier": "FLAVIO FILIPPINI",
+     "companyMail": "filippiniflavio@gmail.com",
+     "companyPhone": "786-663-2376",
+     "companyAddress": "19501 W COUNTRY CLUB DR #1214",
+     "companyCounty": "AVENTURA",
+     "companyState": "FLORIDA",
+     "companyZip": 33180,
+     "licenseType": null,
+     "licenseNumber": "11BS00469",
+     "licenseIssued": null,
+     "licenseExpDate": null
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec31",
+     "companyName": "ARM PROPERTY SERVICES LLC",
+     "qualifier": "MARTINEZ ADDIEL ROGER",
+     "companyMail": null,
+     "companyPhone": "786-436-7062",
+     "companyAddress": "3021 SW 16 ST",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33145,
+     "licenseType": "CRC",
+     "licenseNumber": "1332782",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec32",
+     "companyName": "H&R CONSTRUCTIONS DEVELOPMENT LLC",
+     "qualifier": "HENRY ROMERO",
+     "companyMail": "heroe2007@hotmail.com",
+     "companyPhone": "786-718-7211",
+     "companyAddress": "605 SW 11 ST 6E",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33129,
+     "licenseType": "CGC",
+     "licenseNumber": "1522384",
+     "licenseIssued": null,
+     "licenseExpDate": null
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec33",
+     "companyName": "ALES GROUP ELECTRICAL CONTRACTORS",
+     "qualifier": "LORENTE RAMON",
+     "companyMail": "alesgroupec@gmail.com",
+     "companyPhone": "305-545-6950",
+     "companyAddress": "994 SW 69 AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33144,
+     "licenseType": "EC",
+     "licenseNumber": "0001288",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec34",
+     "companyName": "MESA CONSTRUCTION COMPANY",
+     "qualifier": "MESA TOMAS",
+     "companyMail": null,
+     "companyPhone": "305-978-9611",
+     "companyAddress": "11401 SW 1ST CT",
+     "companyCounty": "PLANTATION",
+     "companyState": "FLORIDA",
+     "companyZip": 33325,
+     "licenseType": "CFC",
+     "licenseNumber": "027531",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec35",
+     "companyName": "A.P. EXPRESS AIR CONDITIONING AND REFRIGERATION INC",
+     "qualifier": "PIEDRA AHMED",
+     "companyMail": "apexpressair@gmail.com",
+     "companyPhone": "786-252-7224",
+     "companyAddress": "8010 W 23 AVE",
+     "companyCounty": "HIALEAH",
+     "companyState": "FLORIDA",
+     "companyZip": 33016,
+     "licenseType": "CAC",
+     "licenseNumber": "1816768",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec36",
+     "companyName": "APPEARANCE BUILDERS LLC",
+     "qualifier": "NARANJO LUIS FELIPE",
+     "companyMail": null,
+     "companyPhone": "786-222-1876",
+     "companyAddress": "2121 SW 3 AVE SUITE 600",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33129,
+     "licenseType": "CGC",
+     "licenseNumber": "1525037",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec37",
+     "companyName": "M V ELECTRICAL SERVICES INC",
+     "qualifier": "VALDES MARIO A",
+     "companyMail": "mario@mvelectrical.com",
+     "companyPhone": "305-216-0677",
+     "companyAddress": "18311 NW 82 CT",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33015,
+     "licenseType": "EC",
+     "licenseNumber": "13005608",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec38",
+     "companyName": "REGOSA ENGINEERING SERVICES INC",
+     "qualifier": "VELEZ GUSTAVO DANIEL",
+     "companyMail": "contractorwork@gmail.com",
+     "companyPhone": "786-262-2964",
+     "companyAddress": "15700 NE 2 AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33162,
+     "licenseType": "CGC",
+     "licenseNumber": "1508952",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec39",
+     "companyName": "REGOSA ENGINEERING SERVICES INC",
+     "qualifier": "VELEZ GUSTAVO DANIEL",
+     "companyMail": "contractorwork@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "15700 NE 2 AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33162,
+     "licenseType": "CCC",
+     "licenseNumber": "1327735",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec3a",
+     "companyName": "REGOSA ENGINEERING SERVICES INC",
+     "qualifier": "VELEZ GUSTAVO DANIEL",
+     "companyMail": "contractorwork@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "15700 NE 2 AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33162,
+     "licenseType": "CFC",
+     "licenseNumber": "1427292",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec3b",
+     "companyName": "REGOSA ENGINEERING SERVICES INC",
+     "qualifier": "VELEZ GUSTAVO DANIEL",
+     "companyMail": "contractorwork@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "15700 NE 2 AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33162,
+     "licenseType": "CMC",
+     "licenseNumber": "1249755",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec3c",
+     "companyName": "REGOSA ENGINEERING SERVICES INC",
+     "qualifier": "VELEZ GUSTAVO DANIEL",
+     "companyMail": "contractorwork@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "15700 NE 2 AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33162,
+     "licenseType": "CUC",
+     "licenseNumber": 1224687,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec3d",
+     "companyName": "STONE GROUP ENT INC",
+     "qualifier": "KAREN AZARI",
+     "companyMail": "karenazari@yahoo.com",
+     "companyPhone": "305-761-1240",
+     "companyAddress": "1195 NW 85 ST",
+     "companyCounty": "NORTH MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33151,
+     "licenseType": null,
+     "licenseNumber": "02B500385",
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec3e",
+     "companyName": "J Y P SERVICES & REPAIRS, CORP",
+     "qualifier": "PENA JOSE Y",
+     "companyMail": null,
+     "companyPhone": "305-216-7283",
+     "companyAddress": "9915 WEST OKEECHOBEE RD APT 5-107",
+     "companyCounty": "HIALEAH GARDENS",
+     "companyState": "FLORIDA",
+     "companyZip": 33016,
+     "licenseType": "CFC",
+     "licenseNumber": 1429883,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec3f",
+     "companyName": "LEON DUCT WORK INC",
+     "qualifier": "RUBIO FRANCISCO",
+     "companyMail": "leonductwork@yahoo.com",
+     "companyPhone": null,
+     "companyAddress": "1194 SW 133 PLACE CIRCLE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33184,
+     "licenseType": "CAC",
+     "licenseNumber": 1821070,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec40",
+     "companyName": "AMBUCON INC",
+     "qualifier": "AMORES ANGEL AMADO",
+     "companyMail": "ambucongc@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "10440 SW 170 ST",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33157,
+     "licenseType": "CGC",
+     "licenseNumber": 1516610,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec41",
+     "companyName": "AMBUCON INC",
+     "qualifier": "AMORES ANGEL AMADO",
+     "companyMail": "ambucongc@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "10440 SW 170 ST",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33157,
+     "licenseType": "CMC",
+     "licenseNumber": 1249813,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec42",
+     "companyName": "AMBUCON INC",
+     "qualifier": "AMORES ANGEL AMADO",
+     "companyMail": "ambucongc@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "10440 SW 170 ST",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33157,
+     "licenseType": "CFC",
+     "licenseNumber": 1427116,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec43",
+     "companyName": "BUILD & FIX INC",
+     "qualifier": "FURTADO PEDRO",
+     "companyMail": "fixplumbing123@aol.com",
+     "companyPhone": null,
+     "companyAddress": "9076 HARDING AVE",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33154,
+     "licenseType": "CGC",
+     "licenseNumber": 1513513,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec44",
+     "companyName": "DRAAM CONSTRUCTION INC",
+     "qualifier": "ARIAS FERNANDO",
+     "companyMail": "draam_construction@yahoo.com",
+     "companyPhone": null,
+     "companyAddress": "7530 SW 36 ST",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33155,
+     "licenseType": "CGC",
+     "licenseNumber": 1513180,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec45",
+     "companyName": "RICHPORT CONSTRUCTION GROUP INC",
+     "qualifier": "TOLEDO ONIEL EDGARDO",
+     "companyMail": null,
+     "companyPhone": null,
+     "companyAddress": "18489 NW 88 PLACE",
+     "companyCounty": "HIALEAH",
+     "companyState": "FLORIDA",
+     "companyZip": 33018,
+     "licenseType": "CGC",
+     "licenseNumber": 1515837,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "6220d7cac39ee6cb02fbec46",
+     "companyName": "CONSTRUCTORA VIANPE INC",
+     "qualifier": "ANGARITA MIGUEL ANGEL",
+     "companyMail": "vianpe.usa@gmail.com",
+     "companyPhone": null,
+     "companyAddress": "16680 S POST APT#201",
+     "companyCounty": "WESTON",
+     "companyState": "FLORIDA",
+     "companyZip": 33331,
+     "licenseType": "CGC",
+     "licenseNumber": 1530523,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "622a3617f479a63e62957e65",
+     "companyName": "AMERICAN STAR CONSTRUCTION INC",
+     "qualifier": "CESPEDES JOSE V",
+     "companyMail": "josecespedes@victronics.org",
+     "companyPhone": null,
+     "companyAddress": "409 PHIPPEN RD",
+     "companyCounty": "DANIA BEACH",
+     "companyState": "FLORIDA",
+     "companyZip": 33004,
+     "licenseType": "CGC",
+     "licenseNumber": 1508888,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    },
+    {
+     "_id": "622a987df479a63e62957e67",
+     "companyName": "CONINMAQ L.L.C.",
+     "qualifier": "GUTIERREZ ANA",
+     "companyMail": "conimaqllc@gmail.com",
+     "companyPhone": "786-285-3073",
+     "companyAddress": "16741 ROYAL POINCIANA DR",
+     "companyCounty": "WESTON",
+     "companyState": "FLORIDA",
+     "companyZip": 33326,
+     "licenseType": "CGC",
+     "licenseNumber": 1514716,
+     "licenseIssued": null,
+     "licenseExpDate": null
+    },
+    {
+     "_id": "622a999af479a63e62957e69",
+     "companyName": "WM CONSTRUCTION GROUP INC",
+     "qualifier": "WAGNER ALBERTO CABRERA",
+     "companyMail": null,
+     "companyPhone": "305-408-6231",
+     "companyAddress": "13484 SW 22 TER ",
+     "companyCounty": "MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33175,
+     "licenseType": "CGC",
+     "licenseNumber": 1516547,
+     "licenseIssued": null,
+     "licenseExpDate": null
+    },
+    {
+     "_id": 1,
+     "companyName": "STONE GROUP ENT INC",
+     "qualifier": "KAREN AZARI",
+     "companyMail": "karenazari@yahoo.com",
+     "companyPhone": "305-761-1240",
+     "companyAddress": "1177 NW 81 ST",
+     "companyCounty": "NORTH MIAMI",
+     "companyState": "FLORIDA",
+     "companyZip": 33150,
+     "licenseType": "CBC",
+     "licenseNumber": 1264921,
+     "licenseIssued": null,
+     "licenseExpDate": "08\/31\/2022"
+    }
+   ]
+
+ 
+  contractor.forEach((el) => {
+    const div = document.createElement('div');
+    // const { companyName, qualifier, companyMail } = el;
+    div.classList.add('card-container');
+  
+    const a = document.createElement('a');
+    a.setAttribute('href',`#`);
+    a.innerHTML = el.companyName;
+    a.dataset.id = el._id;
+    // a.textContent = el.companyName;
+    div.appendChild(a);
+  
+    const divDetalle = document.createElement('div');
+    // const { companyName, qualifier, companyMail } = el;
+    divDetalle.classList.add('card-detail-container');
+    divDetalle.classList.add('hidden');
+
+    const p1 = document.createElement('p');
+    p1.textContent = "Qualifier: " + el.qualifier;
+    divDetalle.appendChild(p1);
+
+    const p2 = document.createElement('p');
+    p2.textContent = "Mail: " + el.companyMail;
+    divDetalle.appendChild(p2);
+
+    const p3= document.createElement('p');
+    p3.textContent = "Phone: " + el.companyPhone;
+    divDetalle.appendChild(p3);
+
+    const p4 = document.createElement('p');
+    p4.textContent = "Address: " + el.companyAddress;
+    divDetalle.appendChild(p4);
+
+    const p5 = document.createElement('p');
+    p5.textContent = "City: " + el.companyCounty;
+    divDetalle.appendChild(p5);
+
+    const p6 = document.createElement('p');
+    p6.textContent = "State: " + el.companyState;
+    divDetalle.appendChild(p6);
+
+    const p7 = document.createElement('p');
+    p7.textContent = "City: " + el.companyZip;
+    divDetalle.appendChild(p7);
+
+    const p8 = document.createElement('p');
+    p8.textContent = "License type: " + el.licenseType;
+    divDetalle.appendChild(p8);
+
+    const p9 = document.createElement('p');
+    p9.textContent = "License #: " + el.licenseNumber;
+    divDetalle.appendChild(p9);
+
+    const p10 = document.createElement('p');
+    p10.textContent = "License Issued: " + el.licenseIssued;
+    divDetalle.appendChild(p10);
+
+    const p11 = document.createElement('p');
+    p11.textContent = "License Exp. Date: " + el.licenseExpDate;
+    divDetalle.appendChild(p11);
+  
+    const container = document.querySelector('.container')
+    // const cardContainer = document.querySelector('.card-container')
+    container.appendChild(div);
+
+    div.appendChild(divDetalle);
+  });
+
+      // Primero Selecciono elementos y les doy el click
+      const contractorNames = document.querySelectorAll('a');
+      for(let contractorName of contractorNames) {
+        // console.log(contractorName.dataset.id)
+        contractorName.addEventListener('click', (e) => {
+        //   const dataId = e.currentTarget.dataset.id;
+        //   const text = e.currentTarget.textContent;
+            // console.log(e.currentTarget.nextElementSibling)
+            const allDetailCards = document.querySelectorAll(`.card-detail-container`)
+            allDetailCards.forEach((card) => {
+                card.classList.add(`hidden`)
+            })
+            e.currentTarget.nextElementSibling.classList.remove(`hidden`)
+            // console.log($(this).parent().children('card-detail-container'));
+            // showCurrentDetail();
+        //   e.currentTarget.classList.remove('hidden');
+        //   mostarFondo();
+        //   mostrarModal(dataId, text);
+        });
+      }
+
+    //   function showCurrentDetail(){}
